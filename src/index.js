ea5cc00224c403e83573a556cf126784edecd5f5
Andrey Sokolovskiy <bugnetsys@gmail.com>
2021-08-22 15:20:18 +0200

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array === `undefined` || array.length === 0 ){
+    return 0;
+  } else {
+    let minElement = array[0];
+    array.forEach(element => {
+        if (minElement > element){
+            minElement = element;
+        }
+    });
+    return minElement;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (typeof array === `undefined` || array.length === 0 ){
+        return 0;
+    } else {
+      let maxElement = array[0];
+      array.forEach(element => {
+        if (maxElement < element){
+            maxElement = element;
+        }
+    });
+    return maxElement;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (typeof array === `undefined` || array.length === 0 ){
+        return 0;
+    } else {
+        let sumElements = 0;
+        array.forEach(element => {
+           sumElements+=element;
+       });
+        return sumElements / array.length;
+    }
 }
+
